34e67bcdcaef9e91999408938fa4f09b5d8de45c
Oliver Pietsch <kontakt@oliver-pietsch.de>
2023-03-04 16:36:12 +0000

    Reduce logging

--- src/index.ts
+++ src/index.ts
@@ -10,19 +10,18 @@ const commitSha = process.env.CI_COMMIT_SHA
 let state: string
 let url: string
 const getDeployment = async (): Promise<void> => {
-  console.log('Attempting to get deployment with:', token, teamId, projectId, commitSha)
+  console.log('Attempting to get deployment')
   const response = await fetch(`https://api.vercel.com/v6/deployments?teamId=${teamId}&projectId=${projectId}&meta-gitlabCommitSha=${commitSha}`, {
     headers: {
       Authorization: `Bearer ${token}`
     }
   })
   const result = await response.json()
-  console.log('Deployment: ', result)
   state = result?.deployments[0].state
   url = result?.deployments[0].url
 
   if (state === 'READY') {
-    console.log('Attempting to write to file', url)
+    console.log('Attempting to write to file: ', url)
     await fs.appendFile('vercel_deployment_url.txt', url, (err) => {
       if (err) {
         console.log('Something went wrong: ', err)
@@ -31,6 +30,7 @@ const getDeployment = async (): Promise<void> => {
     return
   }
 
+  console.log('State not ready: ', state)
   setTimeout(() => getDeployment(), 5000)
 }
 
